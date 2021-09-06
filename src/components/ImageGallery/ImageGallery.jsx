59d28f4baa64fe6f4781766f997a1b729f00ca19
Dima <75967292+DukaDima@users.noreply.github.com>
2021-09-06 10:55:39 +0300

    прокидывает ошибку.

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -10,49 +10,64 @@ export default function ImageGallery({ searchPhoto, onClick }) {
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
+
   const BASE_URL = 'https://pixabay.com/api/';
   const KEY = '22333452-088c943be01bb3bdea991b2c2';
 
   useEffect(() => {
-    if (searchPhoto === '') {
-      return;
-    }
-    setPage(1);
-    setPhotos([]);
-    setLoading(true);
-    fetch(
-      `${BASE_URL}?q=${searchPhoto}&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-    )
-      .then(res => res.json())
-      .then(photos => setPhotos(photos.hits))
-      .catch(error => setError(error))
-      .finally(() => setLoading(false));
+    (async () => {
+      if (searchPhoto === '') {
+        return;
+      }
+      setPage(1);
+      setPhotos([]);
+      setLoading(true);
+      try {
+        await fetch(
+          `${BASE_URL}?q=${searchPhoto}&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+        )
+          .then(res => res.json())
+          .then(photos => setPhotos(photos.hits))
+          .catch(error => {
+            throw new Error('oops');
+          })
+          .finally(() => setLoading(false));
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    })();
   }, [searchPhoto]);
 
   useEffect(() => {
-    if (page === 1) {
-      return;
-    }
-    setLoading(true);
-    try {
-      new Error();
-      fetch(
-        `${BASE_URL}?q=${searchPhoto}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-      )
-        .then(res => res.json())
-        .then(photos => setPhotos(oldPhotos => [...oldPhotos, ...photos.hits]))
-        .catch(error => setError(error))
-        .finally(() => {
-          setLoading(false);
-          window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
+    (async () => {
+      if (page === 1) {
+        return;
+      }
+      setLoading(true);
+      try {
+        await fetch(
+          `${BASE_URL}?q=${searchPhoto}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+        )
+          .then(res => res.json())
+          .then(photos =>
+            setPhotos(oldPhotos => [...oldPhotos, ...photos.hits]),
+          )
+          .catch(error => {
+            throw new Error('oops');
+          })
+          .finally(() => {
+            setLoading(false);
+            window.scrollTo({
+              top: document.documentElement.scrollHeight,
+              behavior: 'smooth',
+            });
           });
-        });
-    } catch (error) {
-      setError(error);
-      setLoading(false);
-    }
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    })();
   }, [page]);
 
   const handleAddPage = () => {
